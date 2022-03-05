887556c5fb61e21fd7a473c8df27da1716a289e1
unknown <tasnimtanzim55@gmail.com>
2022-03-05 20:08:18 +0600

    Weather Details added

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const loadByCityName = () => {
     const cityName = document.getElementById('city-name').value;
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`;
-    // console.log(url);
+    console.log(url);
     fetch(url)
     .then(response => response.json())
     .then(data => showWeather(data));
@@ -23,9 +23,18 @@ const loadByCityName = () => {
 
 // Get dynamic inner text 
 const setInnerText = (id, text) => {
-    document.getElementById(id).innerText = text;
+    document.getElementById(id).innerText =`${text ? text : 'N/A'}`;
 }
-
+// Unix Time to GMT Time 
+const localTime = (unixTime) => {
+    const timeStamp = unixTime;
+    let localDate = new Date(timeStamp * 1000);
+    let hours = localDate.getHours().toString().padStart(2,0) ;
+    let minutes = localDate.getMinutes().toString().padStart(2,0);
+    let seconds = localDate.getSeconds().toString().padStart(2,0);
+    let result = `${hours}:${minutes}:${seconds}`;
+    return result;
+}
 // Show Weather Details 
 const showWeather = (details) => {
     setInnerText('city', details.name);
@@ -34,4 +43,17 @@ const showWeather = (details) => {
     const weatherIcon = document.getElementById('weather-icon');
     const iconUrl = `http://openweathermap.org/img/wn/${details.weather[0].icon}@2x.png`;
     weatherIcon.setAttribute('src', iconUrl);
+
+    setInnerText('feels-like', details.main.feels_like);
+    setInnerText('max-temp', details.main.temp_max);
+    setInnerText('min-temp', details.main.temp_min);
+    setInnerText('cloudiness', details.clouds.all);
+    setInnerText('humidity', details.main.humidity);
+    setInnerText('pressure', details.main.pressure);
+    setInnerText('wind-speed', details.wind.speed);
+    setInnerText('sea-level', details.main.sea_level);  
+    setInnerText('sunrise', localTime(details.sys.sunrise));
+    setInnerText('sunset', localTime(details.sys.sunset));    
 }
+    
+    
